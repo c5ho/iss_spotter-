f773fb2d854903f8e70b965a15ed772e588e014b
Calvin Ho <calvin.ch.ho@gmail.com>
2022-06-10 14:25:24 -0400

    making changes to have index call only one function for ISS flyby times and duration

--- index.js
+++ index.js
@@ -17,42 +17,36 @@ nextISSTimesForMyLocation((error, passTimes) => {
     return console.log("It didn't work!", error);
   }
   //success, print out everthang!
-  //console.log(passTimes.response);
+  // console.log(passTimes.response);
   // console.log('-----------------');
   outputISSTimesForMyLocation(passTimes.response);
 });
 
 const outputISSTimesForMyLocation = function(passTimes) {
-  let datetime = new Date(0);
-  console.log(datetime);
-  //console.log(pass.risetime);
-  console.log(passTimes[0].risetime);
-  
-  console.log(datetime.setUTCSeconds(passTimes[0].risetime));
-  datetime = new Date(0);
-  console.log(datetime.setUTCSeconds(1654898651));
-  
-  
-  console.log(datetime);
-  console.log(`${datetime}`);
-  
-  
-  // for (const pass of passTimes) {
-  //   const datetime = new Date(0);
-  //   console.log(datetime);
-  //   console.log(pass.risetime);
-
-  //   datetime.setUTCSeconds(pass.risetime);
-  //   const duration = pass.duration;
-  //   console.log(`Next pass at ${datetime} for ${duration} seconds!`);
- // }
+  for (const pass of passTimes) {
+    const datetime = new Date(0); //datetime is an obj
+    // console.log(typeof datetime);
+    // console.log(pass.risetime);
+    // let temp = Object.assign({},{name:"Cal"});
+    // console.log(temp);
+    // console.log(typeof temp);
+    datetime.setUTCSeconds(1655151412);
+    // console.log(typeof datetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+ }
 };
 
+/*
+[
+  { risetime: 1654997606, duration: 484 },
+  { risetime: 1655034006, duration: 544 },
+  { risetime: 1655070406, duration: 472 },
+  { risetime: 1655106806, duration: 625 },
+  { risetime: 1655143206, duration: 625 }
+]
 
-
-
-
-
+*/
 
 
 
